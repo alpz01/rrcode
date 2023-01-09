dc8824e7d65f8b5a52ddad5f0170bad5a3241035
Alpaz <91442589+alpz01@users.noreply.github.com>
2023-01-09 10:49:17 +0800

    Create thedogofflanders.js

---
+++ src/watch/thedogofflanders.js
@@ -0,0 +1,17 @@
+s1Ep = [
+   "https://www.blogger.com/video.g?token=AD6v5dw7038QR22e2V6KGfNVODRzisO86lf8RLNYAkgpS2kKx6FFYCVmwS7QxyRmQ-FV-WUF5ke6HYavmJ-pkvKRDDWliywr32Mn21boq2VGB_bfasUuAA966DFgGCOP82ZE9-GmcjSZ",
+   "https://www.blogger.com/video.g?token=AD6v5dx4gJVYeZ4lJpu__8tcJnAJHFFjeFwhVgO9PGNcf7l03xXMvDaADCPg1dx8NTzxvDYuGKmMwrWGObr06Qjz-uG_9UDhEr3iuJ5GECUh5HbGHqy5kb4zddhTkqhAimH5irkZJIfo",
+   "https://www.blogger.com/video.g?token=AD6v5dwevZHwOe5loWdIJ1Y88gbvwRGZfFrTd_6HXwfbnWPB882vdQ3w2lB4fnWmx-j15Sncib24ppkAVINITGZ9UdBgXJHFVn4fOgn-XI8DLyE3Zq8c468nErPptqmMSZHIfBJR7bw",
+   "https://www.blogger.com/video.g?token=AD6v5dxZ9phGkyA6jBIKOFw73gTYJ7vUk826h4mg-_OfA0C8Kl8Wi8qqlrBTHVQFN8fnv0Y7RH-QujfPYCLrpleIvr5XBuO0bq_FQzgvac5QfoHZADUDqowmlZq12XYUXutDRQZDL3s",
+   "https://www.blogger.com/video.g?token=AD6v5dxTeFPmpj8aEgo2PhsgI4kNj_C5dvdDYt4QzzZWJga8c4hQCNgz2gNagJSoAFHt__xLSewFWl0KD9ncr1rP-VSCbaeH0HN-RmNUStDgyC9GohJZOOxDeCEuwpNLh2fC37gsPajX",
+   "https://www.blogger.com/video.g?token=AD6v5dwp1wjHerBCoLOUpReAbm9FExdsqlAKZC-xbuWCboN4Tws_4DIgYDPB5OgJXQeeWPfRaKAVPd50x95i6PUAUGWIwZ5UtU_AC-EAkQDqHUQYJ56ncRcK1nvwJb-l82Tw6OwjL6lS",
+   "https://www.blogger.com/video.g?token=AD6v5dy1hNsSLq6OXR2Lh--vJnMhZwWFcXgMOB8wRa7ZyrDdcgQD5ym-LISzCTdJ7btFoNXhY18L9Sh3Ire9XnwIg2m5uvLvAviGf23Zy8grrIZI01Z_o5P8lzGU1kqHIM2BAsft0Ho",
+   "https://www.blogger.com/video.g?token=AD6v5dxDW5IZ-hxw7Wit2rt9313vuSb0SOFLzdYxD3dAYtfs9mvM_75f-OJmAuoQnI_ZNN_nj-7UzQXb3d-6j9Mrns8zcocQ8dy_gJkOdCoamOplh6ihto_YT3MjKK6FgqnEMW5RPA",
+   "https://www.blogger.com/video.g?token=AD6v5dxQvJxdR32ujB1xcNI-eJvcbET-khfqFo6tGFO9fPiAgZhx9VWd3YNgThNS5lzNrawI58vQB2H6-NfWoGF6DylDtAJ978RCMWtHMxwhpx2v8MRAjfBJy1f9vcIc5Vdqw98xiHaa",
+   "https://www.blogger.com/video.g?token=AD6v5dyqT--5G0snKoKCGnroT4CVpUNTGKa4BorvGBsRBdnh-UmBos_hUXkWh0_dCYqFpB-bXsICDdQcKsZy59V1auja5iiuimh-IzO2LlOm-f_HML3dyT1sLw9stqZIhmyS6gmygpI",
+   "https://www.blogger.com/video.g?token=AD6v5dzFLZalFLROcnPrdwbQW0L6oTExeG43MWjcSbXsd8eI4_e0ckTyVVmmHEzYUiU84FlxuuJvSJh-25H1qDiWlzn8rO1miKmMkbTFBMNYx_KLc74JpTlpQJ0dUuksQKX8H2z1AA0",
+   "https://www.blogger.com/video.g?token=AD6v5dxUemNlj9H76wQwyqTA5sgP3LWkildXc3iFK_7KR5lqu1jgcd8FDvXf5IgCGwIK42EcQSLNs1sQ36dUHqWac9qGS7UVrUt4ocyfRkmGWzOkO3erg1C0E8XkEo7R_HTim771UBg",
+   "https://www.blogger.com/video.g?token=AD6v5dwmn7-3eTB2tDqoX51S8VrEygE7hWE6KOQFogTsU1vLTAvPSJQSMatXeZMD58UTO-XZjJ5Prg5HjUUeNDt2Mk6xYE8KA9IEAyFfyuL3BFVdJmR8Wl8KitUc22Ziw6JilJJScyM",
+]
+
+episodes = s1Ep;
