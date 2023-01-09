2eb9b09aec4c47cd8025e1b3541ba3a00e5a034b
Alpaz <91442589+alpz01@users.noreply.github.com>
2023-01-09 11:10:04 +0800

    Create marco.js

---
+++ src/watch/marco.js
@@ -0,0 +1,14 @@
+s1Ep = [
+ "https://www.blogger.com/video.g?token=AD6v5dzTYQ6Z1saqy--QFe86-grmaxSI5X_sd93S10WVymqrRwLc-HLmR-vP0jpnJhRlS-PvHMNWgD2F82Empmz6X3KI-sqYDWzquWVMXsTVHVv3i6mW7B4Y6r-HPlEdGxYwxIOU8o7C",
+ "https://www.blogger.com/video.g?token=AD6v5dzNX1p6SHwjpDsBtoZvLtJJ9AkVEyx9nhxwhZBvVqRX8GnvHfkd1u2ynth6KA7jieYl7f41evHepbFcYVr8cDNyUjaWca0K-ekZhETlKcQKW92pBuEBNeDgjrTphyc6Wo9pMMEP",
+ "https://www.blogger.com/video.g?token=AD6v5dz-cerOxURwm7uu97Na5GH62NuHveHKKtYkFJViZZYzDMLwIP2h9iHnjKaFsNYHxyT_g9Z0xEmLV8Mv3KJfvBBvqjL08kS4HNCpniAFlsSAxbbSJ52DQH4pUcisWUNZYEWkeNri",
+ "https://www.blogger.com/video.g?token=AD6v5dxWRZmQf-7WLNnatftXea831qMdYBObGGOKJNLQ9MBIQM6Htdd3Hy81-yNy-ZDR5fgO1ET88YcKufXTw__tazi24MVqg1oRAQyjFG-jzm4QqbU1BEi-SvKdMlw1YrQDGUEcKT4",
+ "https://www.blogger.com/video.g?token=AD6v5dzHcrUb9N0PxOdBsVkEfwucGGxfsS4xnIMp4f1XJWhXFIKljv1uzjGLtSlLJ-EG0W_lzybua-qq-ovLGnXNdNagzYrqCXytMRxAsm6u7fapMdUP1eZOBcVTU3KBS_Riy1h1KPcz",
+ "https://www.blogger.com/video.g?token=AD6v5dxcUSyAYBNzmrnRHnPwCfhAto7AYhFiWmfH8X54yCD6OPmhc6rv375wdtKNqOi70S7WfAvc00eXDWM418aodBnahcvcbJVM8Sf4Cs0uhYAfh3tRVeRDbg4slLqGAVg8-HLsHnw",
+ "https://www.blogger.com/video.g?token=AD6v5dxkNEuskJH6942hI60vkoF6EkDdgJizccd6BpjgpLmcYlk5_nH1BtQQr_cKf34MnvR3TYTr_ocz67scHx3wq7JEdEkZfQJITaysrS-m0MXVwIpbsRqV_Vt-q2l-1SBkoBfO7Idb",
+ "https://www.blogger.com/video.g?token=AD6v5dwWgOPznwl7eW5B1F_jg3DLHHh0TFHCUB4cKJRRDFl-XdSZ7tHKfVHHtf14xd0J0fuZIeYSlcTNdeSGzi49eRoc908xOomG9o7JzNSbEsawc_zFYyMT0xJJGvdBnVxhZbI6nWTY",
+ "https://www.blogger.com/video.g?token=AD6v5dzQckrxOrIAD8AWn3v3sgvBTIgn2tgeSTm3swwDbwCwWAONPKVgY84QhTjbbJXVJU6bbpI-G7fsodoqqS5TMmYaES42agsY7JyYRVFedp7NWnlt69C7v40talIfYCh5VPfbmNGM",
+ "https://www.blogger.com/video.g?token=AD6v5dyy5GG9kTXBtvB93PfY3Bl19EzQrMq3Hx-U5_BkvHX2LyysXvvrE7lijwGxiO-V6i4G6WOLqMMzFzAB6G2lLIWrtw8ua8K4ZYcZdI9qFXCIpvHgEKnKFYCVga0wd2rAZNXxfHXH",
+]
+
+episodes = s1Ep;
