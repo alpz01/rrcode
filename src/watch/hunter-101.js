9cfa5bad9b87a610008e62533f437a2cb28b2110
Alpaz <91442589+alpz01@users.noreply.github.com>
2023-02-20 09:00:32 +0800

    Update and rename hunter.js to hunter-101.js

--- src/watch/hunter-101.js
+++ src/watch/hunter-101.js
@@ -4,7 +4,9 @@ for (let i = 1; i <= 148; i++) {
   
   if (i < 61) {
       episodes.push(`${fcode}anime-2.0-5/%F0%9D%97%94%F0%9D%97%BB%F0%9D%97%B6%F0%9D%97%BA%F0%9D%97%B2%20%F0%9D%9F%AE.%F0%9D%9F%AC%20-%20${i}.mp4`)
-  } else if (i > 60) {
+  } else if (i > 60 && i < 102) {
       episodes.push(`${fcode}anime-2.0-100/%F0%9D%97%94%F0%9D%97%BB%F0%9D%97%B6%F0%9D%97%BA%F0%9D%97%B2%20%F0%9D%9F%AE.%F0%9D%9F%AC%20-%20${i}.mp4`)
+  } else if (i > 100) {
+       episodes.push(`${fcode}anime-2.0-114/%F0%9D%97%94%F0%9D%97%BB%F0%9D%97%B6%F0%9D%97%BA%F0%9D%97%B2%20%F0%9D%9F%AE.%F0%9D%9F%AC%20-%20${i}.mp4`)
   }
 }
