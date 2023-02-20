3443c44f05c43d4327e7504cfbf42a96fc78abfc
Alpaz <91442589+alpz01@users.noreply.github.com>
2023-02-20 08:54:37 +0800

    Create hunter.js

---
+++ src/watch/hunter.js
@@ -0,0 +1,10 @@
+let episodes = [];
+for (let i = 1; i <= 148; i++) {
+  let fcode = "https://archive.org/download/";
+  
+  if (i < 61) {
+      episodes.push(`${fcode}anime-2.0-5/%F0%9D%97%94%F0%9D%97%BB%F0%9D%97%B6%F0%9D%97%BA%F0%9D%97%B2%20%F0%9D%9F%AE.%F0%9D%9F%AC%20-%20${i}.mp4`)
+  } else if (i > 60) {
+      episodes.push(`${fcode}anime-2.0-100/%F0%9D%97%94%F0%9D%97%BB%F0%9D%97%B6%F0%9D%97%BA%F0%9D%97%B2%20%F0%9D%9F%AE.%F0%9D%9F%AC%20-%20${i}.mp4`)
+  }
+}
